631abe4e9abd17026f1744014b09b9176f827e9d
CristianM1909 <acevedocristian593@gmail.com>
2024-10-15 10:06:08 -0500

    cambio Auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,20 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Cliente;
+use App\Models\Compra;
+use App\Models\Product;
+use App\Models\Proveedor;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Venta;
+use App\Policies\ClientePolicy;
+use App\Policies\CompraPolicy;
+use App\Policies\ProductPolicy;
+use App\Policies\ProveedorPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,6 +27,13 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
+        Cliente::class => ClientePolicy::class,
+        Product::class => ProductPolicy::class,
+        Compra::class => CompraPolicy::class,
+        Proveedor::class => ProveedorPolicy::class,
+        Role::class => RolePolicy::class,
+        User::class => UserPolicy::class,
+        Venta::class => Venta::class,
         //
     ];
 
